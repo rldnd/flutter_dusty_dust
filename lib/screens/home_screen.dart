8e8585b76e70f63dc08500300bac1d6c233f490a
rldnd <gi981226@gmail.com>
2023-01-28 22:15:51 +0900

    feat(snackbar)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:dusty_dust/components/main_app_bar.dart';
 import 'package:dusty_dust/components/main_drawer.dart';
 import 'package:dusty_dust/constants/regions.dart';
@@ -36,36 +37,42 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Future<void> fetchData() async {
-    final now = DateTime.now();
-    final fetchTime = DateTime(now.year, now.month, now.day, now.hour);
+    try {
+      final now = DateTime.now();
+      final fetchTime = DateTime(now.year, now.month, now.day, now.hour);
 
-    final box = Hive.box<StatModel>(ItemCode.PM10.name);
-    final recent = box.values.last;
+      final box = Hive.box<StatModel>(ItemCode.PM10.name);
+      final recent = box.values.last;
 
-    if (recent.dataTime.isAtSameMomentAs(fetchTime)) return;
+      if (recent.dataTime.isAtSameMomentAs(fetchTime)) return;
 
-    List<Future> futures = [];
+      List<Future> futures = [];
 
-    for (final itemCode in ItemCode.values) {
-      futures.add(StatRepository.getStats(itemCode: itemCode));
-    }
-
-    final results = await Future.wait(futures);
-
-    for (int i = 0; i < results.length; i++) {
-      final ItemCode key = ItemCode.values[i];
-      final List<StatModel> value = results[i];
-
-      final box = Hive.box<StatModel>(key.name);
-      for (StatModel stat in value) {
-        box.put(stat.dataTime.toString(), stat);
+      for (final itemCode in ItemCode.values) {
+        futures.add(StatRepository.getStats(itemCode: itemCode));
       }
 
-      final allKeys = box.keys.toList();
-      if (allKeys.length > 24) {
-        final deleteKeys = allKeys.sublist(0, allKeys.length - 24);
-        box.deleteAll(deleteKeys);
+      final results = await Future.wait(futures);
+
+      for (int i = 0; i < results.length; i++) {
+        final ItemCode key = ItemCode.values[i];
+        final List<StatModel> value = results[i];
+
+        final box = Hive.box<StatModel>(key.name);
+        for (StatModel stat in value) {
+          box.put(stat.dataTime.toString(), stat);
+        }
+
+        final allKeys = box.keys.toList();
+        if (allKeys.length > 24) {
+          final deleteKeys = allKeys.sublist(0, allKeys.length - 24);
+          box.deleteAll(deleteKeys);
+        }
       }
+    } on DioError catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('인터넷 연결이 원활하지 않습니다.'),
+      ));
     }
   }
 
@@ -102,44 +109,49 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           body: Container(
             color: status.primaryColor,
-            child: CustomScrollView(
-              controller: scrollController,
-              slivers: [
-                MainAppBar(
-                  region: region,
-                  stat: recentStat,
-                  status: status,
-                  dateTime: recentStat.dataTime,
-                  isExpanded: isExpanded,
-                ),
-                SliverToBoxAdapter(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      CategoryCard(
-                        region: region,
-                        darkColor: status.darkColor,
-                        lightColor: status.lightColor,
-                      ),
-                      const SizedBox(height: 16.0),
-                      ...ItemCode.values.map(
-                        (itemCode) {
-                          return Padding(
-                            padding: const EdgeInsets.only(bottom: 16.0),
-                            child: HourlyCard(
-                              darkColor: status.darkColor,
-                              lightColor: status.lightColor,
-                              itemCode: itemCode,
-                              region: region,
-                            ),
-                          );
-                        },
-                      ),
-                      const SizedBox(height: 16.0),
-                    ],
+            child: RefreshIndicator(
+              onRefresh: () async {
+                await fetchData();
+              },
+              child: CustomScrollView(
+                controller: scrollController,
+                slivers: [
+                  MainAppBar(
+                    region: region,
+                    stat: recentStat,
+                    status: status,
+                    dateTime: recentStat.dataTime,
+                    isExpanded: isExpanded,
                   ),
-                ),
-              ],
+                  SliverToBoxAdapter(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        CategoryCard(
+                          region: region,
+                          darkColor: status.darkColor,
+                          lightColor: status.lightColor,
+                        ),
+                        const SizedBox(height: 16.0),
+                        ...ItemCode.values.map(
+                          (itemCode) {
+                            return Padding(
+                              padding: const EdgeInsets.only(bottom: 16.0),
+                              child: HourlyCard(
+                                darkColor: status.darkColor,
+                                lightColor: status.lightColor,
+                                itemCode: itemCode,
+                                region: region,
+                              ),
+                            );
+                          },
+                        ),
+                        const SizedBox(height: 16.0),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
